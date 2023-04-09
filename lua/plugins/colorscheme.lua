b437642b56a0a57c5e00660be2a2f09824e094fe
IogaMaster <iogamastercode@gmail.com>
2023-04-09 09:46:15 -0600

    feat(colorscheme): add catppuccin mocha colorscheme
    Changed background from base to crust color.

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    lazy = false,
+    priority = 1000,
+    config = function()
+        require("catppuccin").setup {
+            color_overrides = {
+                all = {
+                    base = "#11111b",
+                }
+            }
+        }
+        vim.cmd [[colorscheme catppuccin-mocha]]
+    end,
+}
