2b30be593ae78691bc226246a3c9d504c9f70cda
IogaMaster <iogamastercode@gmail.com>
2023-04-09 12:39:25 -0600

    refactor(staline): make status line global

--- init.lua
+++ init.lua
@@ -1,6 +1,8 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+vim.opt.laststatus = 3
+
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system {
