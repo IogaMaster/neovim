328601bcb2cf62b0a31c71f8472419bd6fa89532
IogaMaster <iogamastercode@gmail.com>
2023-04-10 14:27:25 -0600

    fix not showing up

--- lua/plugins/barbecue.lua
+++ lua/plugins/barbecue.lua
@@ -3,5 +3,9 @@ return {
     dependencies = {
         "SmiteshP/nvim-navic",
         "nvim-tree/nvim-web-devicons", -- optional dependency
-    }
+    },
+    config = function()
+        require("barbecue").setup {}
+        require("barbecue.ui").toggle(true)
+    end
 }
