ad0864ddad36dd2875293ffa6ddf5e49b15cbdd0
IogaMaster <iogamastercode@gmail.com>
2023-04-10 17:00:51 -0600

    feat(plugins): swap toggleterm for nvterm

---
+++ lua/plugins/nvterm.lua
@@ -0,0 +1,9 @@
+return {
+    "NvChad/nvterm",
+    keys = {
+        { '<leader>ot', '<cmd>lua require("nvterm.terminal").toggle "horizontal"<cr>', desc = 'Toggle Terminal' },
+    },
+    config = function()
+        require("nvterm").setup()
+    end,
+}
