460163a9e31d81e58921c41fd9250d8e9c98311f
IogaMaster <iogamastercode@gmail.com>
2023-04-09 18:39:29 -0600

    Remove netrw

--- init.lua
+++ init.lua
@@ -2,21 +2,24 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 vim.opt.laststatus = 3
+vim.cmd [[
+    let g:loaded_netrw       = 1
+    let g:loaded_netrwPlugin = 1
+]]
 
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system {
-		'git',
-		'clone',
-		'--filter=blob:none',
-		'https://github.com/folke/lazy.nvim.git',
-		'--branch=stable', -- latest stable release
-		lazypath,
-	}
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  }
 end
 vim.opt.rtp:prepend(lazypath)
 require('lazy').setup 'plugins'
-
 
 -- Keymaps
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
