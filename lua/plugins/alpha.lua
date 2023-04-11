98122011ebdafdfd382c761d8b12d44a28016e86
IogaMaster <iogamastercode@gmail.com>
2023-04-10 20:28:29 -0600

    feat(plugins): add alpha.nvim

---
+++ lua/plugins/alpha.lua
@@ -0,0 +1,33 @@
+return {
+    'goolord/alpha-nvim',
+    event = 'VimEnter',
+    config = function()
+        local dashboard = require 'alpha.themes.dashboard'
+        dashboard.section.header.val = {
+            "   ⣴⣶⣤⡤⠦⣤⣀⣤⠆     ⣈⣭⣿⣶⣿⣦⣼⣆          ",
+            "    ⠉⠻⢿⣿⠿⣿⣿⣶⣦⠤⠄⡠⢾⣿⣿⡿⠋⠉⠉⠻⣿⣿⡛⣦       ",
+            "          ⠈⢿⣿⣟⠦ ⣾⣿⣿⣷    ⠻⠿⢿⣿⣧⣄     ",
+            "           ⣸⣿⣿⢧ ⢻⠻⣿⣿⣷⣄⣀⠄⠢⣀⡀⠈⠙⠿⠄    ",
+            "          ⢠⣿⣿⣿⠈    ⣻⣿⣿⣿⣿⣿⣿⣿⣛⣳⣤⣀⣀   ",
+            "   ⢠⣧⣶⣥⡤⢄ ⣸⣿⣿⠘  ⢀⣴⣿⣿⡿⠛⣿⣿⣧⠈⢿⠿⠟⠛⠻⠿⠄  ",
+            "  ⣰⣿⣿⠛⠻⣿⣿⡦⢹⣿⣷   ⢊⣿⣿⡏  ⢸⣿⣿⡇ ⢀⣠⣄⣾⠄   ",
+            " ⣠⣿⠿⠛ ⢀⣿⣿⣷⠘⢿⣿⣦⡀ ⢸⢿⣿⣿⣄ ⣸⣿⣿⡇⣪⣿⡿⠿⣿⣷⡄  ",
+            " ⠙⠃   ⣼⣿⡟  ⠈⠻⣿⣿⣦⣌⡇⠻⣿⣿⣷⣿⣿⣿ ⣿⣿⡇ ⠛⠻⢷⣄ ",
+            "      ⢻⣿⣿⣄   ⠈⠻⣿⣿⣿⣷⣿⣿⣿⣿⣿⡟ ⠫⢿⣿⡆     ",
+            "       ⠻⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⡟⢀⣀⣤⣾⡿⠃     ",
+        }
+        dashboard.section.header.opts = {
+            position = "center",
+            hl = "SpecialComment",
+        }
+
+        require('alpha').setup(require('alpha.themes.dashboard').config)
+        dashboard.section.buttons.val = {
+            dashboard.button('SPC p p', ' Open Project', '<CMD>Telescope projections<CR>'),
+            dashboard.button('SPC f f', ' Find File', '<CMD>Telescope find_files<CR>'),
+            dashboard.button('SPC f r', ' Recent Files', '<CMD>Telescope oldfiles<CR>'),
+            dashboard.button('SPC f t', '󰗧 Find in Files', '<CMD>Telescope live_grep<CR>'),
+        }
+    end,
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+}
