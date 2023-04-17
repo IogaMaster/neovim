045fd2d4104018e9195050a994117b1f8e1f431a
IogaMaster <iogamastercode@gmail.com>
2023-04-17 10:47:09 -0600

    Formatting

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,34 +1,34 @@
 return {
-	"goolord/alpha-nvim",
-	enabled = false,
-	event = "VimEnter",
-	config = function()
-		local dashboard = require("alpha.themes.dashboard")
-		dashboard.section.header.val = {
-			"   ⣴⣶⣤⡤⠦⣤⣀⣤⠆     ⣈⣭⣿⣶⣿⣦⣼⣆          ",
-			"    ⠉⠻⢿⣿⠿⣿⣿⣶⣦⠤⠄⡠⢾⣿⣿⡿⠋⠉⠉⠻⣿⣿⡛⣦       ",
-			"          ⠈⢿⣿⣟⠦ ⣾⣿⣿⣷    ⠻⠿⢿⣿⣧⣄     ",
-			"           ⣸⣿⣿⢧ ⢻⠻⣿⣿⣷⣄⣀⠄⠢⣀⡀⠈⠙⠿⠄    ",
-			"          ⢠⣿⣿⣿⠈    ⣻⣿⣿⣿⣿⣿⣿⣿⣛⣳⣤⣀⣀   ",
-			"   ⢠⣧⣶⣥⡤⢄ ⣸⣿⣿⠘  ⢀⣴⣿⣿⡿⠛⣿⣿⣧⠈⢿⠿⠟⠛⠻⠿⠄  ",
-			"  ⣰⣿⣿⠛⠻⣿⣿⡦⢹⣿⣷   ⢊⣿⣿⡏  ⢸⣿⣿⡇ ⢀⣠⣄⣾⠄   ",
-			" ⣠⣿⠿⠛ ⢀⣿⣿⣷⠘⢿⣿⣦⡀ ⢸⢿⣿⣿⣄ ⣸⣿⣿⡇⣪⣿⡿⠿⣿⣷⡄  ",
-			" ⠙⠃   ⣼⣿⡟  ⠈⠻⣿⣿⣦⣌⡇⠻⣿⣿⣷⣿⣿⣿ ⣿⣿⡇ ⠛⠻⢷⣄ ",
-			"      ⢻⣿⣿⣄   ⠈⠻⣿⣿⣿⣷⣿⣿⣿⣿⣿⡟ ⠫⢿⣿⡆     ",
-			"       ⠻⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⡟⢀⣀⣤⣾⡿⠃     ",
-		}
-		dashboard.section.header.opts = {
-			position = "center",
-			hl = "Comment",
-		}
+  'goolord/alpha-nvim',
+  enabled = false,
+  event = 'VimEnter',
+  config = function()
+    local dashboard = require 'alpha.themes.dashboard'
+    dashboard.section.header.val = {
+      '   ⣴⣶⣤⡤⠦⣤⣀⣤⠆     ⣈⣭⣿⣶⣿⣦⣼⣆          ',
+      '    ⠉⠻⢿⣿⠿⣿⣿⣶⣦⠤⠄⡠⢾⣿⣿⡿⠋⠉⠉⠻⣿⣿⡛⣦       ',
+      '          ⠈⢿⣿⣟⠦ ⣾⣿⣿⣷    ⠻⠿⢿⣿⣧⣄     ',
+      '           ⣸⣿⣿⢧ ⢻⠻⣿⣿⣷⣄⣀⠄⠢⣀⡀⠈⠙⠿⠄    ',
+      '          ⢠⣿⣿⣿⠈    ⣻⣿⣿⣿⣿⣿⣿⣿⣛⣳⣤⣀⣀   ',
+      '   ⢠⣧⣶⣥⡤⢄ ⣸⣿⣿⠘  ⢀⣴⣿⣿⡿⠛⣿⣿⣧⠈⢿⠿⠟⠛⠻⠿⠄  ',
+      '  ⣰⣿⣿⠛⠻⣿⣿⡦⢹⣿⣷   ⢊⣿⣿⡏  ⢸⣿⣿⡇ ⢀⣠⣄⣾⠄   ',
+      ' ⣠⣿⠿⠛ ⢀⣿⣿⣷⠘⢿⣿⣦⡀ ⢸⢿⣿⣿⣄ ⣸⣿⣿⡇⣪⣿⡿⠿⣿⣷⡄  ',
+      ' ⠙⠃   ⣼⣿⡟  ⠈⠻⣿⣿⣦⣌⡇⠻⣿⣿⣷⣿⣿⣿ ⣿⣿⡇ ⠛⠻⢷⣄ ',
+      '      ⢻⣿⣿⣄   ⠈⠻⣿⣿⣿⣷⣿⣿⣿⣿⣿⡟ ⠫⢿⣿⡆     ',
+      '       ⠻⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⡟⢀⣀⣤⣾⡿⠃     ',
+    }
+    dashboard.section.header.opts = {
+      position = 'center',
+      hl = 'Comment',
+    }
 
-		require("alpha").setup(require("alpha.themes.dashboard").config)
-		dashboard.section.buttons.val = {
-			dashboard.button("SPC p p", " Open Project", "<CMD>Telescope projections<CR>"),
-			dashboard.button("SPC f f", " Find File", "<CMD>Telescope find_files<CR>"),
-			dashboard.button("SPC f r", " Recent Files", "<CMD>Telescope oldfiles<CR>"),
-			dashboard.button("SPC f t", "󰗧 Find in Files", "<CMD>Telescope live_grep<CR>"),
-		}
-	end,
-	dependencies = { "nvim-tree/nvim-web-devicons" },
+    require('alpha').setup(require('alpha.themes.dashboard').config)
+    dashboard.section.buttons.val = {
+      dashboard.button('SPC p p', ' Open Project', '<CMD>Telescope projections<CR>'),
+      dashboard.button('SPC f f', ' Find File', '<CMD>Telescope find_files<CR>'),
+      dashboard.button('SPC f r', ' Recent Files', '<CMD>Telescope oldfiles<CR>'),
+      dashboard.button('SPC f t', '󰗧 Find in Files', '<CMD>Telescope live_grep<CR>'),
+    }
+  end,
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
 }
