2143c256cd5757d4f2070f054ba3c3a2946997d3
IogaMaster <iogamastercode@gmail.com>
2023-04-17 10:43:27 -0600

    Add null-ls

---
+++ lua/plugins/null-ls.lua
@@ -0,0 +1,11 @@
+return {
+    "jose-elias-alvarez/null-ls.nvim",
+    config = function()
+        local null_ls = require("null-ls")
+        null_ls.setup({
+            sources = {
+                null_ls.builtins.formatting.stylua,
+            },
+        })
+    end
+}
