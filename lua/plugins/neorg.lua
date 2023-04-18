3d257097aa12225e6537a6602e38a2bb3f14bca0
IogaMaster <iogamastercode@gmail.com>
2023-04-18 15:34:49 -0600

    Neorg telescope integration

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -1,18 +1,19 @@
 return {
-  'nvim-neorg/neorg',
-  build = ':Neorg sync-parsers',
-  dependencies = { { 'nvim-lua/plenary.nvim' } },
-  opts = {
-    load = {
-      ['core.defaults'] = {}, -- Loads default behaviour
-      ['core.norg.concealer'] = {}, -- Adds pretty icons to your documents
-      ['core.norg.dirman'] = { -- Manages Neorg workspaces
-        config = {
-          workspaces = {
-            notes = '~/Documents/notes',
-          },
+    'nvim-neorg/neorg',
+    build = ':Neorg sync-parsers',
+    dependencies = { { 'nvim-lua/plenary.nvim' }, { "nvim-neorg/neorg-telescope" } },
+    opts = {
+        load = {
+            ['core.defaults'] = {},       -- Loads default behaviour
+            ['core.norg.concealer'] = {}, -- Adds pretty icons to your documents
+            ["core.integrations.telescope"] = {},
+            ['core.norg.dirman'] = {      -- Manages Neorg workspaces
+                config = {
+                    workspaces = {
+                        notes = '~/Documents/notes',
+                    },
+                },
+            },
         },
-      },
     },
-  },
 }
