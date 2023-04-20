017bf655bb54e3fd87dfbae44f1b9d5262a10e7c
IogaMaster <iogamastercode@gmail.com>
2023-04-19 21:50:11 -0600

    Make bufferline offset when neotree open

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -2,5 +2,16 @@ return {
   'akinsho/bufferline.nvim',
   version = 'v3.*',
   dependencies = 'nvim-tree/nvim-web-devicons',
-  config = true,
+  config = {
+    options = {
+      offsets = {
+        {
+          filetype = 'neo-tree',
+          text = 'NeoTree',
+          text_align = 'left',
+          separator = true,
+        },
+      },
+    },
+  },
 }
