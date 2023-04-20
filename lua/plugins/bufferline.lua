742bec6c98a79c1f08f7aca93196a14dcb8d33df
IogaMaster <iogamastercode@gmail.com>
2023-04-19 21:34:07 -0600

    Add config for bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,1 +1,6 @@
-return { 'akinsho/bufferline.nvim', version = 'v3.*', dependencies = 'nvim-tree/nvim-web-devicons' }
+return {
+  'akinsho/bufferline.nvim',
+  version = 'v3.*',
+  dependencies = 'nvim-tree/nvim-web-devicons',
+  config = true,
+}
