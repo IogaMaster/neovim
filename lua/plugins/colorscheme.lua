82331249b427a48b100e305e77087321893b760d
IogaMaster <iogamastercode@gmail.com>
2023-05-18 13:43:24 -0600

    Add nord theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,16 +1,25 @@
 return {
-  'catppuccin/nvim',
-  name = 'catppuccin',
-  lazy = false,
-  priority = 1000,
-  config = function()
-    require('catppuccin').setup {
-      color_overrides = {
-        all = {
-          base = '#11111b',
-        },
-      },
-    }
-    vim.cmd [[colorscheme catppuccin-mocha]]
-  end,
+    {
+        'catppuccin/nvim',
+        name = 'catppuccin',
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require('catppuccin').setup {
+                color_overrides = {
+                    all = {
+                        base = '#11111b',
+                    },
+                },
+            }
+            vim.cmd [[colorscheme catppuccin-mocha]]
+        end,
+    },
+    {
+        'shaunsingh/nord.nvim',
+        name = 'nord',
+        lazy = false,
+        priority = 1000,
+    },
+
 }
