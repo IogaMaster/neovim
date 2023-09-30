64516800ba55d7b3a0a041f133e40f223eb9e571
IogaMaster <iogamastercode@gmail.com>
2023-09-30 14:57:01 -0600

    feat: Change neovim logo in neocord

--- lua/plugins/neocord.lua
+++ lua/plugins/neocord.lua
@@ -1,3 +1,6 @@
 return {
-  'IogaMaster/neocord',
+    'IogaMaster/neocord',
+    config = {
+        logo = "https://raw.githubusercontent.com/IogaMaster/neovim/main/.github/assets/nixvim-dark.webp", -- Main image display (either "neovim" or "file")
+    }
 }
