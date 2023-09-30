40e3c893f0438c477b9e41022ac34eced4b2ca13
IogaMaster <iogamastercode@gmail.com>
2023-09-29 19:44:48 -0600

    feat(lazy): Use local dev versions of plugins when available

--- init.lua
+++ init.lua
@@ -23,4 +23,10 @@ if not vim.loop.fs_stat(lazypath) then
   }
 end
 vim.opt.rtp:prepend(lazypath)
-require('lazy').setup 'plugins'
+require('lazy').setup {
+  import = 'plugins',
+  dev = {
+    path = '~/dev',
+    fallback = true,
+  },
+}
---
+++ lua/plugins/neocord.lua
@@ -0,0 +1,3 @@
+return {
+  'IogaMaster/neocord',
+}
