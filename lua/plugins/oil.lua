6d2dc15133e975549dad5e9c998a294c2a5eb6b1
IogaMaster <iogamastercode@gmail.com>
2023-10-03 13:13:04 -0600

    feat(oil): Remove auto preview as it sucks

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -10,16 +10,5 @@ return {
     require('oil').setup {
       skip_confirm_for_simple_edits = true,
     }
-
-    -- Auto Preview
-    vim.api.nvim_create_autocmd('User', {
-      pattern = 'OilEnter',
-      callback = vim.schedule_wrap(function(args)
-        local oil = require 'oil'
-        if vim.api.nvim_get_current_buf() == args.data.buf and oil.get_cursor_entry() then
-          oil.select { preview = true }
-        end
-      end),
-    })
   end,
 }
