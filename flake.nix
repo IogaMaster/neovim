72e694fad059ac6d22245d9dfb74eea86172dd16
IogaMaster <iogamastercode@gmail.com>
2023-10-17 22:58:56 -0600

    feat: Add wakatime-cli to neovim runtime

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
             "--prefix"
             "PATH"
             ":"
-            "${lib.makeBinPath [ pkgs.gcc pkgs.nil pkgs.chafa pkgs.ffmpegthumbnailer ]}"
+            "${lib.makeBinPath [ pkgs.gcc pkgs.nil pkgs.chafa pkgs.ffmpegthumbnailer pkgs.wakatime ]}"
           ];
         });
     in
