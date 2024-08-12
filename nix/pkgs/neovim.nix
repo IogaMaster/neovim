ff3a7a347a7efc09348832941ed1d84e5536b7e0
IogaMaster <iogamastercode@gmail.com>
2024-08-11 19:53:58 -0600

    feat(pkgs/neovim): fhs support
    Allows for mason.nvim to work when I add that

--- nix/pkgs/neovim.nix
+++ nix/pkgs/neovim.nix
@@ -8,50 +8,63 @@
   clang,
   pkg-config,
   cargo,
+  buildFHSEnv,
   bundled ? true,
 }: let
-  config = let
-    extraPackages = [
-      # Rocks.nvim deps
-      lua5_1
-      luarocks
-      clang
-      pkg-config
-      cargo
-    ];
-  in
-    neovimUtils.makeNeovimConfig
-    {
-      withPython3 = false;
-      withRuby = false;
-      withNodeJs = false;
-
-      extraLuaPackages = p:
-        with p; [
-          magick
-        ];
-
-      inherit extraPackages;
-
-      customRC =
-        if bundled
-        then ''
-          set runtimepath^=${../../.}
-          source ${../../.}/init.lua
-        ''
-        else ''
-          set runtimepath^=~/.nvim
-          set runtimepath-=~/.config/nvim
-          source ~/.nvim/init.lua
-        '';
-    }
-    // {
-      wrapperArgs = [
-        "--prefix"
-        "PATH"
-        ":"
-        "${lib.makeBinPath extraPackages}"
+  nvim = let
+    config = let
+      extraPackages = [
+        # Rocks.nvim deps
+        lua5_1
+        luarocks
+        clang
+        pkg-config
+        cargo
       ];
-    };
+    in
+      neovimUtils.makeNeovimConfig
+      {
+        withPython3 = false;
+        withRuby = false;
+        withNodeJs = false;
+
+        extraLuaPackages = p:
+          with p; [
+            magick
+          ];
+
+        inherit extraPackages;
+
+        customRC =
+          if bundled
+          then ''
+            set runtimepath^=${../../.}
+            source ${../../.}/init.lua
+          ''
+          else ''
+            set runtimepath^=~/.nvim
+            set runtimepath-=~/.config/nvim
+            source ~/.nvim/init.lua
+          '';
+      }
+      // {
+        wrapperArgs = [
+          "--prefix"
+          "PATH"
+          ":"
+          "${lib.makeBinPath extraPackages}"
+        ];
+      };
+  in
+    wrapNeovimUnstable neovim-unwrapped config;
 in
-  wrapNeovimUnstable neovim-unwrapped config
+  buildFHSEnv {
+    name = "nvim-fhs";
+    targetPkgs = pkgs: [
+      nvim
+    ];
+
+    runScript = ''
+      nvim
+    '';
+  }
