084c4a9b7d5bc37057596641a1655cdec9e20a13
IogaMaster <iogamastercode@gmail.com>
2026-06-16 22:44:04 -0600

    fix treesitter textobjects

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,31 +1,36 @@
 return {
   'nvim-treesitter',
-  lazy =false, -- nice to have this early
+  lazy = false,
   pkgs = {
     'nvim-treesitter/nvim-treesitter',
     'nvim-treesitter/nvim-treesitter-textobjects',
   },
-  before = function()
-    vim.cmd 'TSUpdate'
-  end,
   after = function()
     require('nvim-treesitter').setup {
       auto_install = true,
       highlight = { enable = true },
     }
-    require('nvim-treesitter.configs').setup {
-      textobjects = {
-        select = {
-          enable = true,
-          lookahead = true,
-          keymaps = {
-            ['af'] = '@function.outer',
-            ['if'] = '@function.inner',
-            ['ac'] = '@class.outer',
-            ['ic'] = '@class.inner',
-          },
-        },
+    require("nvim-treesitter-textobjects").setup {
+      select = {
+        lookahead = true, -- Automatically jump forward to textobj
+        include_surrounding_whitespace = false,
       },
     }
+    vim.keymap.set({ "x", "o" }, "af", function()
+      require "nvim-treesitter-textobjects.select".select_textobject("@function.outer", "textobjects")
+    end, { desc = "Around function" })
+
+    vim.keymap.set({ "x", "o" }, "if", function()
+      require "nvim-treesitter-textobjects.select".select_textobject("@function.inner", "textobjects")
+    end, { desc = "Inside function" })
+
+    -- Class Selectors
+    vim.keymap.set({ "x", "o" }, "ac", function()
+      require "nvim-treesitter-textobjects.select".select_textobject("@class.outer", "textobjects")
+    end, { desc = "Around class" })
+
+    vim.keymap.set({ "x", "o" }, "ic", function()
+      require "nvim-treesitter-textobjects.select".select_textobject("@class.inner", "textobjects")
+    end, { desc = "Inside class" })
   end,
 }
